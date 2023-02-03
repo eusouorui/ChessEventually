23495163a9a30638b1567a490351549057756842
eusouorui <ruippdias99@gmail.com>
2023-02-03 19:12:09 +0000

    create mvc app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ChessEventually.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
